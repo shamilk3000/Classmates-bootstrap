b5637b3ccd86491aef001a7803419b0b807fd0f8
shamilk3000 <shamilk3000@gmail.com>
2024-10-30 16:44:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,5 +12,4 @@ function showSlides() {
   if (slideIndex > slides.length) {slideIndex = 1}    
   
   slides[slideIndex-1].style.display = "block";  
-  setTimeout(showSlides, 5000); // Change image every 2 seconds
-}
+  setTimeout(showSlides, 5000); 
